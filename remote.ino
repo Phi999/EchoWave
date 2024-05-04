13405b5abc6eda9426ca8e63c7d5cf0938026767
Phil999 <72974980+Phi999@users.noreply.github.com>
2024-05-04 10:52:00 +0300

    Update remote.ino

--- remote.ino
+++ remote.ino
@@ -1,48 +1,37 @@
-#include <RH_ASK.h>
-#include <SPI.h>
-
-RH_ASK rf_driver;
-int normalx = 350;
-int normaly = 350;
-int step = 70;
+#include <Nunchuk.h>
+#include <Wire.h> 
 void setup() {
-  Serial.begin(9600);
+  // put your setup code here, to run once:
+      Serial.begin(9600); 
+	   Wire.begin(); 
+       //  Wire.setClock(400000);
 
-  // Initialize ASK Object
-  rf_driver.init();
+	   nunchuk_init(); 
+
+
 }
 
 void loop() {
-  delay(200);
-  int xValue = analogRead(A5);
-  int yValue = analogRead(A4);
-  //Preia datele de la joystick si le trimite prin radio
-    Serial.print("x = ");
-  Serial.print(xValue);
-  Serial.print(", y = ");
-  Serial.println(yValue);
-  const char *msg = "no";
-  if(xValue > normalx + step){
-    msg = "up";
-  }
-  if(xValue < normalx - step){
-    msg = "dw";
-  } 
-  if(yValue < normaly-step)
-  {
-  msg = "lt";
-  tone(8, 3600, 250);
-  delay(200);
-  noTone(8);
-  
-  }
-  if(yValue > normaly+step){
-    msg = "rt";
-          tone(8, 3000, 250);
-      delay(200);
-          noTone(8);
-  }
-  Serial.println(msg);
-  rf_driver.send((uint8_t *)msg, strlen(msg));
-  rf_driver.waitPacketSent();
+  // put your main code here, to run repeatedly:
+  	   if (nunchuk_read()) { 
+	       // Work with nunchuk_data 
+
+	       int x =  nunchuk_joystickX();
+         int y =  nunchuk_joystickY();
+         int angle = nunchuk_joystick_angle();
+         int pitch = nunchuk_pitch();
+         int roll = nunchuk_roll();
+         Serial.print(x);
+         Serial.print(" ");
+         Serial.print(y);
+         Serial.print(" ");
+         Serial.print(angle);
+         Serial.print(" ");
+         Serial.print(pitch);
+         Serial.print(" ");
+         Serial.println(roll);
+	   } 
+	   delay(10); 
+
+
 }
