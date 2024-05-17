bd8698d1b78c2ce88d2cdcaf1d703507d4cff3a3
Phil999 <72974980+Phi999@users.noreply.github.com>
2024-05-17 19:06:36 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,13 +1,15 @@
-#include <RH_ASK.h>
-#include <SPI.h>
+#include <RH_ASK.h>//libraria RadioHead, pentru modulul Wireles
+#include <SPI.h>//librarie folosita de libraria RadioHead
 
-RH_ASK rf_driver;//creaza o instanta a radioului
+RH_ASK rf_driver;//creaza o instanta a modulului wireles
 //Pini pentru motoare
+
 int motor1pin1 = 2;
 int motor1pin2 = 3;
 
 int motor2pin1 = 4;
 int motor2pin2 = 5;
+//functii pentru controlarea fiecarui motor
 void motor1(){
   digitalWrite(motor1pin1,  HIGH);
   digitalWrite(motor1pin2, LOW);
@@ -33,7 +35,7 @@ void motor2b(){
   digitalWrite(motor2pin1,  LOW);
   digitalWrite(motor2pin2, HIGH);
 }
-
+//functii pentru fata, spate, stanga, dreapta
 void fw(){
   motor1();
   motor2();
@@ -56,8 +58,9 @@ void right(){
   motor2s();
 }
 void setup() {
-  rf_driver.init();
-  Serial.begin(9600);
+  rf_driver.init();//intializam modulul wireles
+  Serial.begin(9600);//folosim Serial port pentru debugging
+  //setam pini motoarelor ca OUTPUT
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
   pinMode(motor2pin1, OUTPUT);
@@ -65,47 +68,50 @@ void setup() {
 }
 
 void handleDirection(char direction) {
+//functia handledirection primeste un character si in functie de
+//acel character ii spune barcii in ce directie sa mearga
   //Directii
   switch (direction) {
     case 'u':
       Serial.println("Moving Forward");
+      //fata
       fw();
       break;
     case 'd':
       Serial.println("Moving Backwards");
+      //spate
       bw();
       break;
     case 'l':
       Serial.println("Moving Left");
-        tone(8, 3000, 250);
-        delay(200);
-        noTone(8);
+      //stanga
       left();
       break;
     case 'r':
       Serial.println("Moving Right");
-        tone(8, 4000, 250);
-        delay(200);
-        noTone(8);
+      //dreapta
       right();
       break;
     default:
+    //daca nu e nici una dintre cele de mai sus, barca se opreste
       Serial.println("STOP");
       stp();
   }
 }
 
 void loop() {
-  uint8_t buf[2];
+
+  uint8_t buf[2];//mesajul ce il voi primi
   uint8_t buflen = sizeof(buf);
-  if (rf_driver.recv(buf, &buflen)) {
+  if (rf_driver.recv(buf, &buflen)) {//daca primesc un mesaj 
     Serial.print("Message Received: ");
     //Afiseaza mesajul primit
     for (int i = 0; i < buflen; i++) {
       Serial.print((char)buf[i]);
     }
     Serial.println();
-    handleDirection((char)buf[0]); 
+    handleDirection((char)buf[0]); //trimit prima litera a mesajului functiei handleDirection()
+
   }
-  delay(200);
+  delay(200);//delay pentru a nu suprasolicita placa arduino
 }
